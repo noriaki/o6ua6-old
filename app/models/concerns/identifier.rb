4840c2edf9cdb2be7282b710a0cc92a3561190cf
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-01-23 19:17:17 +0900

    change interface Identifier

--- app/models/concerns/identifier.rb
+++ app/models/concerns/identifier.rb
@@ -1,9 +1,13 @@
 module Identifier
   extend ActiveSupport::Concern
 
+  def self.identify(value)
+    CGI.escape(value).gsub('%', '').downcase.reverse.to_s
+  end
+
   class_methods do
     def identify(value)
-      CGI.escape(value).gsub('%', '').downcase.reverse.to_s
+      Identifier.identify(value)
     end
   end
 end
