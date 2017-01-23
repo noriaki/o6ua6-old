06382601b95396e92c84df295281b3c730ce135c
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-01-23 13:40:00 +0900

    add data seeds
    from https://github.com/noriaki/gengo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+File.open(Rails.root.join('db', 'KanjiCandidates.json')) do |file|
+  JSON.load(file).each do |term|
+    Kanji.where(surface: term.to_s).first_or_create
+  end
+end
