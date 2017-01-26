cefa404896fd1aaea72fe9689a0995446c82769e
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-01-26 12:31:36 +0900

    enhance dup method, move to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  protected
+  def extract_ids(excepts)
+    excepts.is_a?(String) ? excepts.split('/') : excepts
+  end
 end
