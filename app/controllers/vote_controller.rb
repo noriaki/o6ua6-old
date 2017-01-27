80b45120cf3024ed6db3a395d31dfaa3afc69a32
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-01-27 17:05:47 +0900

    change creating Vote

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -5,6 +5,7 @@ class VoteController < ApplicationController
     @winner = Gengo.where(identifier: winner_id).first
     @loser = Gengo.where(identifier: loser_id).first
     if @winner.won(@loser)
+      Vote.apply(session[:uid], @winner, @loser)
       head :created#, location: gengo_path(@winner) # TODO
     else
       head :bad_request
