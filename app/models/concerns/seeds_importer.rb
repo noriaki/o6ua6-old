8e2562a4631dba4bb68e17d03cabe11ea2baa12b
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-02-01 10:57:12 +0900

    fix lint

--- app/models/concerns/seeds_importer.rb
+++ app/models/concerns/seeds_importer.rb
@@ -1,17 +1,20 @@
 module SeedsImporter
-  @@default_options = { bypass_document_validation: true, ordered: false }
+  DEFAULT_BULK_INSERT_OPTIONS = {
+    bypass_document_validation: true, ordered: false
+  }.freeze
 
   extend ActiveSupport::Concern
 
   class_methods do
-    def import_from_json(filepath, defaults = {}, options = @@default_options)
+    def import_from_json(filepath, defaults = {}, options = DEFAULT_BULK_INSERT_OPTIONS)
       File.open(filepath) do |file|
-        operations = JSON.load(file).map{|item|
+        operations = JSON.parse(file.read).map do |item|
           {
             identifier: Identifier.identify(item), surface: item.to_s
           }.reverse_merge(defaults)
-        }
-        self.collection.insert_many(operations, options)
+        end
+        pp options
+        collection.insert_many(operations, options)
       end
     end
   end
