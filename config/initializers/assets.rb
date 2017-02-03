729e5c7e0d0c85f1a58eeeff8406bf1a19f28ce1
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-02-03 15:48:19 +0900

    enhance asset_path to read manifest.json in app/assets/config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,13 @@
 Rails.application.config.action_controller.asset_host = proc do
   Rails.env.production? ? "#{Aws::S3.bucket_name}.s3.amazonaws.com" : 'localhost:8080'
 end
+
+module AssetUrlHelperWithManifest
+  def compute_asset_path(source, options = {})
+    File.open(Rails.root.join('app', 'assets', 'config', 'manifest.json')) do |manifest|
+      source = JSON.parse(manifest.read)[source.to_s] || source
+    end
+    super(source, options)
+  end
+end
+ActionView::Base.prepend AssetUrlHelperWithManifest
