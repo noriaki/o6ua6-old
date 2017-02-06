1cbd2522d9334f7f53e31ab3e32018c33f37961a
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-02-06 20:45:24 +0900

    add linking rails data and js app

--- app/frontend/js/AppHome.jsx
+++ app/frontend/js/AppHome.jsx
@@ -4,24 +4,7 @@ import Vs from 'Components/Vs';
 
 console.log('AppHome');
 
-const gengos = [
-  {
-    surface: '入物',
-    yomi: ['ニュウブツ'],
-    imageUrls: [
-      'https://o6ua6.s3.amazonaws.com/images/5a585e.jpg',
-      'https://o6ua6.s3.amazonaws.com/images/9a987e.jpg',
-    ],
-  },
-  {
-    surface: '令送',
-    yomi: ['レイソウ'],
-    imageUrls: [
-      'https://o6ua6.s3.amazonaws.com/images/4abb4e.jpg',
-      'https://o6ua6.s3.amazonaws.com/images/18089e.jpg',
-    ],
-  },
-];
+const { gengos } = window.o6ua6;
 
 ReactDOM.render(
   <Vs gengos={gengos} />,
