bf62ba2f97ead9622d111408ab8de8573bd2016e
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-02-06 20:44:55 +0900

    add camelize option

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @gengos = Gengo.limit(2).random
-    gon.gengos = @gengos.map { |g| GengoSerializer.new(g) }
+    gon.gengos = @gengos.map { |g| GengoSerializer.new(g).to_h }
   end
 end
