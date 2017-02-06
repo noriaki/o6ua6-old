a9f1c601f649cdac3b2dad315568615565ede81d
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-02-06 12:58:30 +0900

    add babel config to webpack.config

--- app/frontend/webpack.config.js
+++ app/frontend/webpack.config.js
@@ -18,4 +18,13 @@ module.exports = {
     output: path.resolve('..', 'assets', 'config', 'manifest.json'),
     writeToDisk: true,
   })],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
