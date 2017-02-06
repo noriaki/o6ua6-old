fcb98270073bf2159b1672f307e6af5eda21847f
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-02-06 18:20:10 +0900

    add simple component Gengo vs Gengo

--- app/frontend/js/AppHome.jsx
+++ app/frontend/js/AppHome.jsx
@@ -1,3 +1,29 @@
-const fill = require('lodash/fill');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Vs from 'Components/Vs';
+
 console.log('AppHome');
-console.log(fill(Array(5), 'home'));
+
+const gengos = [
+  {
+    surface: '入物',
+    yomi: ['ニュウブツ'],
+    imageUrls: [
+      'https://o6ua6.s3.amazonaws.com/images/5a585e.jpg',
+      'https://o6ua6.s3.amazonaws.com/images/9a987e.jpg',
+    ],
+  },
+  {
+    surface: '令送',
+    yomi: ['レイソウ'],
+    imageUrls: [
+      'https://o6ua6.s3.amazonaws.com/images/4abb4e.jpg',
+      'https://o6ua6.s3.amazonaws.com/images/18089e.jpg',
+    ],
+  },
+];
+
+ReactDOM.render(
+  <Vs gengos={gengos} />,
+  document.getElementById('content')
+);
