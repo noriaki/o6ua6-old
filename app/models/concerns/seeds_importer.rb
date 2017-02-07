73c9c5d59038210fb064369fffe2904b8dac1b6f
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-02-07 14:19:13 +0900

    add `yomi` field into Gengo

--- app/models/concerns/seeds_importer.rb
+++ app/models/concerns/seeds_importer.rb
@@ -8,12 +8,13 @@ module SeedsImporter
   class_methods do
     def import_from_json(filepath, defaults = {}, options = DEFAULT_BULK_INSERT_OPTIONS)
       File.open(filepath) do |file|
-        operations = JSON.parse(file.read).map do |item|
+        operations = JSON.parse(file.read).map do |item, attrs|
+          attrs = {} if attrs.nil?
+          attrs = { yomi: attrs } if attrs.is_a? Array
           {
             identifier: Identifier.identify(item), surface: item.to_s
-          }.reverse_merge(defaults)
+          }.merge(attrs).reverse_merge(defaults)
         end
-        pp options
         collection.insert_many(operations, options)
       end
     end
