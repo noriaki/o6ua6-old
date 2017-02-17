b5442d2e3098e6f7d432e0337625f757afd175b4
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2017-02-17 11:32:05 +0900

    enhance combineStates

--- app/frontend/js/Reducers/index.jsx
+++ app/frontend/js/Reducers/index.jsx
@@ -1,14 +1,28 @@
+import isPlainObject from 'lodash/isPlainObject';
+import flatten from 'lodash/flatten';
+import compact from 'lodash/compact';
 import { combineReducers } from 'redux';
 import better from 'better-combine-reducers';
 
 import gengoReducers, { initialState as gengoInitialState } from './Gengo';
 
 export const combineStates = (initialState, ...preloadStates) => {
-  const combinedKeys = Object.keys(initialState);
+  const combinedKeys = compact([
+    ...Object.keys(initialState),
+    ...flatten(preloadStates.map(Object.keys)),
+  ]);
   return preloadStates.reduce((finalState, preloadState) => {
     combinedKeys.forEach((key) => {
-      // eslint-disable-next-line no-param-reassign
-      finalState[key] = { ...finalState[key], ...preloadState[key] };
+      /* eslint-disable no-param-reassign */
+      if (isPlainObject(finalState[key]) && isPlainObject(preloadState[key])) {
+        finalState[key] = { ...finalState[key], ...preloadState[key] };
+      } else if (preloadState[key] !== undefined) {
+        if (finalState[key] === undefined) {
+          /* WARN: Key(${key}) should be defined as the initial state */
+        }
+        finalState[key] = preloadState[key];
+      }
+      /* eslint-enable no-param-reassign */
     });
     return finalState;
   }, initialState);
